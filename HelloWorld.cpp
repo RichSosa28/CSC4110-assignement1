b387f90a6a065c4f339c316f8319ee5dbba29c97
Richard Bolaji <richstorm781@gmail.com>
2021-01-27 10:23:25 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,6 @@
-// Simple C++ program to display "Hello World" 
+// Simple C++ program to display "Hello World"
+//Author: Richard Bolaji <gh0865@wayne.edu>
+
 // Header file for input output functions 
 #include<iostream>  
   
